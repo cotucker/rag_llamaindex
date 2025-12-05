178ecb1cf5a3f1575218f7df634abbc8119c9998
cotucker <klimilya005@gmail.com>
2025-12-05 14:43:43 +0300

    Add Gemini captioning, PDF extraction, and deps

--- src/doc_parser.py
+++ src/doc_parser.py
@@ -1,5 +1,25 @@
-import pymupdf
-doc = pymupdf.open("data/Климович Илья 5ПИ Лабораторная 2.pdf")
-for page in doc:
-  text = page.get_text()
-  print(text)
+import fitz
+import io
+from PIL import Image
+from src.image_captioning import caption_image
+
+file = "data/The Rust Programming Language.pdf"
+pdf_file = fitz.open(file)
+
+for page_index in range(len(pdf_file)):
+    page = pdf_file.load_page(page_index)  # load the page
+    image_list = page.get_images(full=True)  # get images on the page
+
+    if image_list:
+        print(f"[+] Found a total of {len(image_list)} images on page {page_index}")
+    else:
+        continue
+
+    for image_index, img in enumerate(image_list, start=1):
+        xref = img[0]
+        base_image = pdf_file.extract_image(xref)
+        image_bytes = base_image["image"]
+        image_ext = base_image["ext"]
+        image_name = f"image{page_index+1}_{image_index}.{image_ext}"
+        print(f"[+] Image {image_name}")
+        print(caption_image(image_bytes))
---
+++ src/image_captioning.py
@@ -0,0 +1,43 @@
+from google import genai
+from google.genai import types
+import os
+from typing import cast
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+
+class Image(BaseModel):
+    image_type: str = Field(description="Type of the image, e.g., 'Picture', 'Drawing', 'Plot', etc.")
+    image_name: str = Field(description="Name or title of the image.")
+    image_description: str = Field(description="A brief description of the image content.")
+
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+def caption_image(image_data: bytes):
+    response = client.models.generate_content(
+        model='gemini-flash-lite-latest',
+        contents=[
+            types.Part.from_bytes(
+            data=image_data,
+            mime_type='image/jpeg',
+            ),
+            'Caption this image.'
+        ],
+        config={
+            "response_mime_type": "application/json",
+            "response_schema": Image,
+        },
+    )
+    image: Image = cast(Image, response.parsed)
+    return image
+
+if __name__ == "__main__":
+
+    with open('barplot_9_0.png', 'rb') as f:
+        image_bytes = f.read()
+
+    caption = caption_image(image_bytes)
+    print("Image Type:", caption.image_type)
+    print("Image Name:", caption.image_name)
+    print("Image Description:", caption.image_description)
